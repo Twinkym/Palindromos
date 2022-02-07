bf9864cc2f0c7542ef9ea23b58b06196f1f91349
david de la puente enriquez <Twinkymen@hotmail.com>
2022-02-07 11:31:09 +0100

    Revisat

--- src/Palindromos.java
+++ src/Palindromos.java
@@ -4,13 +4,10 @@ public class Palindromos {
     public static void main(String[] args) {
 
         String word1 = askForWord();
-        String word2 = askForWord2();
-        showWords(word1, word2);
-        comparewords();
+        String reversedWord = reverseString(word1);
+        showWords(word1);
+        compareWords(word1, reversedWord);
     }
-
-
-
 
     private static String askForWord() {
         System.out.println("Introdueix una paraula:");
@@ -19,18 +16,22 @@ public class Palindromos {
 
     }
 
-    private static String askForWord2() {
-        System.out.println("Introdueix un altra paraula:");
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
+    private static void showWords(String word1) {
+        System.out.println("Paraula: " + word1 );
     }
 
-    private static void showWords(String word1, String word2) {
-        System.out.println("Primera paraula: " + word1 );
-        System.out.println("Segona paraula: " + word2);
+    private static String reverseString(String word1) {
+        String reversedWord = new StringBuilder(word1).reverse().toString();
+        return reversedWord;
     }
 
-    private static void comparewords() {
+    private static String compareWords(String word1, String reversedword) {
+        if ( word1.equalsIgnoreCase(reversedword)){
+            System.out.println("Aquesta paraula es un palindrom!!!!");
 
+        }else{
+            System.out.println("Aquesta paraula no es palindrom!!!!");
+        }
+        return "Gracias por participar";
     }
 }
